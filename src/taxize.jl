df17183abc06bf0d7bf87b763063d8d3822d7c47
Scott Chamberlain <myrmecocystus@gmail.com>
2014-05-04 07:42:16 -0700

    added an example

--- src/taxize.jl
+++ src/taxize.jl
@@ -4,6 +4,12 @@ module Taxize
 
   export itisPing
 
+  """
+  Some notes
+
+  Usage:
+    Taxize.itisPing()
+  """
   function itisPing()
     temp = Requests.get("http://www.itis.gov/ITISWebService/services/ITISService/getDescription")
     return temp.data
